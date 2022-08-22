0bb2eb7e7b82de32573eb1c4ca4aee2bdf38bf9c
Jose Melendez <jomeg08@gmail.com>
2022-08-22 13:08:14 -0500

    The first phase is done by entering TextView and button

--- app/src/main/java/com/josemelendez/courseofkotlin/MainActivity.kt
+++ app/src/main/java/com/josemelendez/courseofkotlin/MainActivity.kt
@@ -2,10 +2,21 @@ package com.josemelendez.courseofkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val tvComencemos = findViewById<TextView>(R.id.tv3)
+
+        findViewById<Button>(R.id.btn1).setOnClickListener {
+            tvComencemos.visibility = View.VISIBLE
+
+            findViewById<TextView>(R.id.tv2).text = "Tu puedes Jose!"
+        }
     }
 }
